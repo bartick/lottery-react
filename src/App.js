e46eefc5ff06bf7ee3f5e6c7ac0394794a0f326b
bartick <bartickmaiti1998@gmail.com>
2022-03-19 15:22:06 +0530

    Changed the class object from function to class

--- src/App.js
+++ src/App.js
@@ -1,84 +1,84 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import { web3, lottery } from './logic'
 
-function App() {
+class App extends Component {
+  
+  constructor (props){
+    super(props);
+    this.state = {
+      "manager" : '', 
+      players   : [], 
+      balance   : '',
+      value     : '',
+      message   : '',
+    };
+  }
 
+  async componentDidMount(){
+    const manager = await lottery.methods.manager().call();//never set  manager var
+    const player  = await lottery.methods.playersData().call();
+    const balance = await web3.eth.getBalance(lottery.options.address);
 
-  const [accounts, setAccounts] = useState({
-    manager : '', 
-    players   : [], 
-    balance   : '',
-    value     : '',
-    message   : '',
-  });
-
-  useEffect(() => {
-    async function setWeb3() {
-      const [manager, players, balance] = await Promise.all([
-        await lottery.methods.manager().call(),
-        await lottery.methods.playersData().call(),
-        await web3.eth.getBalance(lottery.options.address)
-      ]);
-
-      setAccounts({
-        manager : manager,
-        players : players,
-        balance : balance,
-      });
-    }
-    setWeb3();
-
-  });
-
-  const onClick = async (_event) => {
-    const accounts = await web3.eth.getAccounts();
-    setAccounts({message : 'Picking a RANDOM winner........just hold on few secs.....'})
-    await lottery.methods.pickWinner().send({
-      from : accounts[0],
+    this.setState({
+      manager : manager,
+      players : player,
+      balance : balance,
     });
-    setAccounts({message : 'A winner has been picked! And he has recieved the money'})
-  };
+  }
 
-  const onSubmit = async (event) => {
+  onSubmit = async (event) => {
     event.preventDefault();
     const accounts = await web3.eth.getAccounts();
 
-    setAccounts({ message : 'Waiting on transaction success .....' });
+    this.setState({ message : 'Waiting on transaction success .....' });
 
     await lottery.methods.enter().send({
       from : accounts[0],
-      value: web3.utils.toWei(this.accounts.value, 'ether'),
+      value: web3.utils.toWei(this.state.value, 'ether'),
     });
 
-    setAccounts( {message: 'Congratulations!!! You have successfully entered the lottery!' })
+    this.setState( {message: 'Congratulations!!! You have successfully entered the lottery!' })
 
   }
 
-  return (
-    <div>
+  onClick = async (event) => {
+    const accounts = await web3.eth.getAccounts();
+    this.setState({message : 'Picking a RANDOM winner........just hold on few secs.....'})
+    await lottery.methods.pickWinner().send({
+      from : accounts[0],
+    });
+    this.setState({message : 'A winner has been picked! And he has recieved the money'})
+  }
+
+  render() {
+    return (
+      <div>
       <h2>Lottery contract</h2>
-      <p>This contract is managed by {accounts.manager}</p>
-      <p>There are currently {accounts.players.length} people entered, competing to win {web3.utils.fromWei(accounts.balance, 'ether')} ether!!!</p>
+      <p>This contract is managed by {this.state.manager}</p>
+      <p>There are currently {this.state.players.length} people entered, competing to win {web3.utils.fromWei(this.state.balance, 'ether')} ether!!!</p>
       <hr/>
-      <form onSubmit={onSubmit}>
+      <form onSubmit={this.onSubmit}>
         <h4>Want to try your luck?</h4>
         <div>
           <label>Amount of ether to enter : </label>
           <input
-            value={accounts.value}
-            onChange={event => setAccounts({ value : event.target.value })}
+            value={this.state.value}
+            onChange={event => this.setState({ value : event.target.value })}
           />
         </div>
         <button>Enter</button>
       </form>
       <hr/>
       <h4>Ready to Pick a Winner? </h4>
-      <button onClick={onClick} >Pick a Winner!</button>
+      <button onClick={this.onClick} >Pick a Winner!</button>
       <hr/>
-      <h3>{accounts.message}</h3>
+      <h3>{this.state.message}</h3>
     </div>
-  );
+    );
+  }
 }
 
-export default App
+
+
+export default App;
